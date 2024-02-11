ee600168d6f9d048de6942abe4de140b4e92dc24
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-02-11 12:30:32 -0400

    feat: add skeleton for exemplum Lua module

---
+++ lua/exemplum.nvim/init.lua
@@ -0,0 +1,18 @@
+---@mod exemplum exemplum.nvim
+---
+---@brief [[
+---
+---Take your functions and easily refactor them
+---while keeping an eye at the initial implementation
+---
+---@brief ]]
+
+local exemplum = {}
+
+---Set up exemplum.nvim
+---@param user_configs table User configurations
+function exemplum.setup(user_configs)
+  --- TBD
+end
+
+return exemplum
