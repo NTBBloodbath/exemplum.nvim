a55dbe42e6651623ed6c424d9f9a123275b6fb2a
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-02-15 08:44:20 -0400

    feat: add `winbuf` module (empty atm)

---
+++ lua/exemplum/winbuf.lua
@@ -0,0 +1,11 @@
+---@mod exemplum.win exemplum.nvim window and buffer handling
+---
+---@brief [[
+---
+---Window and buffer management module of exemplum.nvim
+---
+---@brief ]]
+
+local winbuf = {}
+
+return winbuf
