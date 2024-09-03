af54057c4d3d770e02c8330cb43ac842d273a08e
NTBBloodbath <bloodbathalchemist@protonmail.com>
2024-09-03 01:13:00 -0400

    chore(exemplum): some small improvements to annotations

--- lua/exemplum/init.lua
+++ lua/exemplum/init.lua
@@ -13,7 +13,13 @@
 ---
 ---The ':Exemplum' command accepts the following arguments:
 --- 'code_type' - The type of code chunk to refactor.
----               Currently it can be `function` or `variable`.
+---               Currently it can be either `enum`, `struct`,
+---               `function` or `variable`.
+---
+---The ':Exemplum' command also accepts a bang modifier (`!`).
+---At the moment if you add the bang modifier, exemplum will
+---disable the diagnostics in the refactoring buffer.
+---
 ---@brief ]]
 
 local exemplum = {}
